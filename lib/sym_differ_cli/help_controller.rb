a41a540cdbabbebac722d9b68d3b6b548d998321
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-02 20:08:12 +0900

    New HelpController

---
+++ lib/sym_differ_cli/help_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module SymDifferCli
+  # Displays help related to the SymDifferCli utility.
+  class HelpController
+    Response = Struct.new(:message)
+
+    def display_help
+      Response.new(render_view)
+    end
+
+    private
+
+    def render_view
+      <<~MESSAGE
+        Usage: sym_differ_cli [options] [--]
+        Executes the specified mathematical operation against the provided input.
+
+          --help, -[hH]    Prints a brief explanation on the usage of the sym_differ_cli
+                           command.
+
+        The command's homepage is located at HOME_PAGE. You can report issues there.
+      MESSAGE
+    end
+  end
+end
---
+++ spec/sym_differ_cli/help_controller_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "sym_differ_cli/help_controller"
+
+RSpec.describe SymDifferCli::HelpController do
+  describe "#display_help" do
+    subject(:display_help) do
+      described_class.new.display_help
+    end
+
+    it { is_expected.to have_attributes(message: <<~MESSAGE) }
+      Usage: sym_differ_cli [options] [--]
+      Executes the specified mathematical operation against the provided input.
+
+        --help, -[hH]    Prints a brief explanation on the usage of the sym_differ_cli
+                         command.
+
+      The command's homepage is located at HOME_PAGE. You can report issues there.
+    MESSAGE
+  end
+end
